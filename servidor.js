5a56f87edbcd773b48100f2892d5fac2e44f1230
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-18 22:25:49 -0500

    Configurando puertos para el servidor

--- servidor.js
+++ servidor.js
@@ -8,6 +8,6 @@ function sitioWeb(req, res){
 let servidor = http.createServer(sitioWeb);
 
 // En que direccion usar mi servidor
-servidor.listen(6969,"127.0.0.1");
+servidor.listen(6969,"192.168.8.100");
 
 console.log("servidor corriendo en el puerto 6969");
